494c625dbb76f2c38ffeb8e079fe98d2d0733348
Derek Sparks <drsparks@gmail.com>
2020-04-23 19:54:25 -0700

    Initial Import

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string ageText = Console.ReadLine();
+
+            bool isValidAge = int.TryParse(ageText, out int currentAge);
+            if (isValidAge)
+            {
+                int kAge = currentAge - 20;
+                if (kAge < 21)
+                {
+                    Console.WriteLine("You weren't alive in 2000");
+                }
+                else
+                {
+                    Console.WriteLine($"You were { kAge } in the year 2000");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
